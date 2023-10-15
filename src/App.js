917ecefb9c8c59f7d5f3ff2d6822c3fb41c8e46b
Yuanuanuan <asdll6618@gmail.com>
2023-10-15 11:12:55 +0800

    feat: AuthContext

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import './styles/reset.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <AuthProvider> */}
+      <AuthProvider>
         <Routes>
           <Route path="*" element={<LoginPage />} />
           <Route path='/home' element={<HomePage />} />
         </Routes>
-      {/* </AuthProvider> */}
+      </AuthProvider>
     </BrowserRouter>
   );
 }
